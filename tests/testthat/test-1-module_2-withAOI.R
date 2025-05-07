5eb52c7573e7aa77c8a3fdbd3ef8e5e976cdf289
Susan Murray <murray.e.susan@gmail.com>
2025-05-07 10:27:53 -0700

    test path update

--- tests/testthat/test-1-module_2-withAOI.R
+++ tests/testthat/test-1-module_2-withAOI.R
@@ -33,7 +33,7 @@ test_that("Module runs with study AOI", {
 
       require = c("sf", "terra"),
 
-      dbPath     = file.path(spadesTestPaths$temp$inputs, "cbm_defaults_v1.2.8340.362.db"),
+      dbPath     = file.path(spadesTestPaths$temp$inputs, "dbPath.db"),
       ecoLocator = sf::st_read(file.path(spadesTestPaths$testdata, "ecoLocator.shp"), quiet = TRUE),
       spuLocator = sf::st_read(file.path(spadesTestPaths$testdata, "spuLocator.shp"), quiet = TRUE),
       disturbanceMatrix = read.csv(file.path(spadesTestPaths$testdata, "disturbance_matrix_association.csv")),
