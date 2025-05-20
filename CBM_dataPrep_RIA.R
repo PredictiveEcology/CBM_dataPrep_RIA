c09a2ca6b432f20ebaf84f81f5c4861fe047462a
Susan Murray <murray.e.susan@gmail.com>
2025-05-20 15:52:27 -0700

    harvest1 & harvest2 scenario tests added

--- CBM_dataPrep_RIA.R
+++ CBM_dataPrep_RIA.R
@@ -13,7 +13,7 @@ defineModule(sim, list(
   #documentation = list("CBM_dataPrep_RIA.Rmd"),
   reqdPkgs = list(
     "data.table", "sf", "terra",
-    "PredictiveEcology/CBMutils@development (>=2.0.3)"
+    "PredictiveEcology/CBMutils@development (>=2.0.3.0001)"
   ),
   parameters = rbind(
     defineParameter("resampling", "character", default = "mode", NA, NA, "Raster resampling method"),
--- tests/testthat/test-1-module_1-RIA-small_harvest2.R
+++ tests/testthat/test-1-module_1-RIA-small_harvest2.R
@@ -3,5 +3,163 @@ if (!testthat::is_testing()) source(testthat::test_path("setup.R"))
 
 test_that("Module: RIA-small - harvest2", {
 
+  ## Run simInit and spades ----
+
+  # Set times
+  times <- list(start = 2020, end = 2099)
+
+  # Set project path
+  projectPath <- file.path(spadesTestPaths$temp$projects, "1-RIA-small_harvest2")
+  dir.create(projectPath)
+  withr::local_dir(projectPath)
+
+  # Set master raster CRS
+  masterRasterCRS <- terra::crs(
+    paste(readLines(file.path(spadesTestPaths$testdata, "masterRasterCRS.prj")), collapse = "\n"))
+
+  # Set up project
+  simInitInput <- SpaDEStestMuffleOutput(
+
+    SpaDES.project::setupProject(
+
+      times = times,
+
+      modules = "CBM_dataPrep_RIA",
+      paths   = list(
+        projectPath = projectPath,
+        modulePath  = spadesTestPaths$modulePath,
+        packagePath = spadesTestPaths$packagePath,
+        inputPath   = spadesTestPaths$inputPath,
+        cachePath   = spadesTestPaths$cachePath,
+        outputPath  = file.path(projectPath, "outputs")
+      ),
+
+      require = c("sf", "terra", "reproducible"),
+
+      ecoLocator = sf::st_read(file.path(spadesTestPaths$testdata, "ecoLocator.shp"), quiet = TRUE),
+      spuLocator = sf::st_read(file.path(spadesTestPaths$testdata, "spuLocator.shp"), quiet = TRUE),
+
+      masterRaster = terra::rast(
+        vals = 1L,
+        res  = 250,
+        ext  = c(xmin = -1653000, xmax = -1553000, ymin = 7765000, ymax = 7865000),
+        crs  = masterRasterCRS
+      ),
+
+      disturbanceMeta = data.table(
+        eventID    = c(1, 2),
+        name       = c("wildfire", "harvest"),
+        disturbance_type_id = c(1, 204),
+        wholeStand = 1
+      ),
+
+      disturbanceRasters = {
+
+        tsas <- c(16, 40)
+
+        reproducible::prepInputs(
+          destinationPath = file.path(spadesTestPaths$inputPath, "harvest2"),
+          url        = "https://drive.google.com/file/d/1PiDpeYGZJfKUPvMGlWvXkEfuThX-lD5r",
+          targetFile = "tif_scenrio-carbon-less_20210622.tar.gz",
+          fun        = utils::untar
+        ) |> Cache()
+
+        list(
+          `1` = lapply(setNames(times$start:times$end, times$start:times$end), function(year){
+            file.path(spadesTestPaths$inputPath, "harvest2", "tif", paste0("tsa", tsas), paste0("projected_fire_",    year, ".tif"))
+          }),
+          `2` = lapply(setNames(times$start:times$end, times$start:times$end), function(year){
+            file.path(spadesTestPaths$inputPath, "harvest2", "tif", paste0("tsa", tsas), paste0("projected_harvest_", year, ".tif"))
+          })
+        )
+      }
+    )
+  )
+
+  # Run simInit
+  simTestInit <- SpaDEStestMuffleOutput(
+    SpaDES.core::simInit2(simInitInput)
+  )
+
+  expect_s4_class(simTestInit, "simList")
+
+  # Run spades
+  simTest <- SpaDEStestMuffleOutput(
+    SpaDES.core::spades(simTestInit)
+  )
+
+  expect_s4_class(simTest, "simList")
+
+
+  ## Check output 'standDT' ----
+
+  expect_true(!is.null(simTest$standDT))
+  expect_true(inherits(simTest$standDT, "data.table"))
+
+  for (colName in c("pixelIndex", "area", "spatial_unit_id")){
+    expect_true(colName %in% names(simTest$standDT))
+    expect_true(all(!is.na(simTest$standDT[[colName]])))
+  }
+
+  expect_identical(data.table::key(simTest$standDT), "pixelIndex")
+
+
+  ## Check output 'cohortDT' ----
+
+  expect_true(!is.null(simTest$cohortDT))
+  expect_true(inherits(simTest$cohortDT, "data.table"))
+
+  for (colName in c("cohortID", "pixelIndex", "gcids", "ages")){
+    expect_true(colName %in% names(simTest$cohortDT))
+  }
+
+  expect_identical(data.table::key(simTest$cohortDT), "cohortID")
+
+  # Check spinup ages are all >= 2
+  expect_true("ageSpinup" %in% names(simTest$cohortDT))
+  expect_equal(simTest$cohortDT$ageSpinup[simTest$cohortDT$ages >= 2],
+               simTest$cohortDT$ages[simTest$cohortDT$ages >= 2])
+  expect_true(all(simTest$ageSpinup[simTest$cohortDT$ages < 2] == 2))
+
+
+  ## Check output 'curveID' ----
+
+  expect_true(!is.null(simTest$curveID))
+  expect_true(length(simTest$curveID) >= 1)
+  expect_true("gcids" %in% simTest$curveID)
+  expect_true(all(simTest$curveID %in% names(simTest$cohortDT)))
+
+
+  ## Check output 'userGcM3' ----
+
+  expect_true(!is.null(simTest$userGcM3))
+  expect_true(inherits(simTest$userGcM3, "data.table"))
+
+  for (colName in c("gcids", "Age", "MerchVolume")){
+    expect_true(colName %in% names(simTest$userGcM3))
+    expect_true(all(!is.na(simTest$userGcM3[[colName]])))
+  }
+
+
+  ## Check output 'disturbanceEvents' -----
+
+  expect_true(!is.null(simTest$disturbanceEvents))
+  expect_true(inherits(simTest$disturbanceEvents, "data.table"))
+
+  for (colName in c("pixelIndex", "year", "eventID")){
+    expect_true(colName %in% names(simTest$disturbanceEvents))
+    expect_true(is.integer(simTest$disturbanceEvents[[colName]]))
+    expect_true(all(!is.na(simTest$disturbanceEvents[[colName]])))
+  }
+
+  expect_true(all(simTest$disturbanceEvents$pixelIndex %in% simTest$standDT$pixelIndex))
+  expect_true(all(simTest$disturbanceEvents$year       %in% start(simTest):end(simTest)))
+
+
+  ## Check output 'disturbanceMeta' ----
+
+  expect_true(!is.null(simTest$disturbanceMeta))
+  expect_true(inherits(simTest$disturbanceMeta, "data.table"))
+
 })
 
